725da312243e0f60bcfc74d07538a1fac02e2134
Lhyshining <luhy97@163.com>
2022-10-16 14:50:14 +0800

    修正

--- JavaFundamentals/src/HelloWorld.java
+++ JavaFundamentals/src/HelloWorld.java
@@ -5,5 +5,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        byte num1 = 10;
+        long num2 = 10L;
+        boolean num3 = false;
+        float num4 = 50.1F;
+        // 二进制0b 十进制 八进制0 十六进制0x
+        int i = 0b10;
+        int i2 = 10;
+        int i3 = 010;
+        int i4 = 0x10;
+        System.out.println(i);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
     }
 }
