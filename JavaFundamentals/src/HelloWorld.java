903e4f2e10a8aba0ba235deefb8928ec2c022ad0
Lhyshining <luhy97@163.com>
2022-10-16 17:36:39 +0800

    java基础-类型转换 date:20221016

--- JavaFundamentals/src/HelloWorld.java
+++ JavaFundamentals/src/HelloWorld.java
@@ -4,7 +4,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // 数据类型部分
         byte num1 = 10;
         long num2 = 10L;
         boolean num3 = false;
@@ -19,5 +19,36 @@ public class HelloWorld {
         System.out.println(i3);
         System.out.println(i4);
 
+        // 浮点数测试
+        System.out.println("=======================");
+        float f1 = 0.1f;
+        double d1 = 1 / 10;
+        System.out.println(f1 == d1); // 永远为false
+        float f2 = 123123123213213123f;
+        float f3 = f2 + 1;
+        System.out.println(f2 == f3); // true
+
+        // 字符串测试
+        System.out.println("=======================");
+        char c1 = 'a';
+        char c2 = '中';
+        System.out.println(c1);
+        System.out.println((int)c1); // 强制转换为unicode
+        System.out.println(c2);
+        System.out.println((int)c2);
+        // unicode编码范围：U0000~UFFFF
+        char c3 = '\u0061'; // 0x61 = 97
+        System.out.println(c3);
+        // 转义字符
+        // \t 制表符
+        // \n 换行
+        System.out.println("Hello\tworld");
+
+        // boolen测试
+        System.out.println("=======================");
+        boolean flag = true;
+        if (flag){
+
+        }
     }
 }
