d16c3b7aac1936941882b392d55cb004ccfcdc0e
Lhyshining <luhy97@163.com>
2022-10-18 10:48:08 +0800

    java基础 date:20221018

--- JavaFundamentals/src/Variable.java
+++ JavaFundamentals/src/Variable.java
@@ -1,5 +1,22 @@
 public class Variable {
-    public static void main(String[] args) {
+    // 类变量 static
+    static double salary = 100;
 
+    // 常量
+    static final double PI = 3.14;
+
+    // 实例变量
+    String name; // 默认null
+    int age; // 默认0
+
+    public static void main(String[] args) {
+        // 局部变量
+        int i = 10;
+        System.out.println(i);
+        Variable variable = new Variable();
+        System.out.println(variable.age);
+        System.out.println(variable.name);
+        System.out.println(salary);
+        System.out.println(PI);
     }
 }
