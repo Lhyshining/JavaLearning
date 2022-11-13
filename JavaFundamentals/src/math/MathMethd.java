fffcff51f8222d35cd53491415f38da1c054764c
Lhyshining <luhy97@163.com>
2022-11-13 20:05:21 +0800

    javaMath date:20221111

---
+++ JavaFundamentals/src/math/MathMethd.java
@@ -0,0 +1,14 @@
+package math;
+
+public class MathMethd {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(9));
+        System.out.println(Math.pow(2, 4));
+        System.out.println(Math.ceil(4.1));
+        System.out.println(Math.floor(4.1));
+        System.out.println(Math.round(4.1));
+        System.out.println(Math.sqrt(4));
+        // random() [0,1)
+        System.out.println(Math.random() * 5 + 2);
+    }
+}
