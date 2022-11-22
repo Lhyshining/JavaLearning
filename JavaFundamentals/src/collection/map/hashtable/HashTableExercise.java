7dbab0ed8371f9c2dd07aac2300afd7c27579bf4
Lhyshining <luhy97@163.com>
2022-11-22 20:37:10 +0800

    Map date:20221122

--- JavaFundamentals/src/collection/map/hashtable/HashTableExercise.java
+++ JavaFundamentals/src/collection/map/hashtable/HashTableExercise.java
@@ -1,7 +1,15 @@
 package collection.map.hashtable;
 
+import java.util.Hashtable;
+
 public class HashTableExercise {
     public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+        hashtable.put(1, "john");
+//        hashtable.put(null,"john"); // error
+//        hashtable.put(2,null); // error
+        hashtable.put(1, "mark");
+        System.out.println(hashtable);
 
     }
 }
