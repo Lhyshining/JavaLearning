cac3c99529577fa85252c4d93300d55d69dad5e6
Lhyshining <luhy97@163.com>
2022-11-25 20:27:26 +0800

    Sort date:20221125

--- Algorithm/src/sort/BubbleSort.java
+++ Algorithm/src/sort/BubbleSort.java
@@ -1,35 +1,24 @@
 package sort;
 
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {9, 2, 3, 2, 1, 7, 3, 10, 2};
-        bubbleSort(arr);
-        printArray(arr);
+        new BubbleSort().bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            // 全程无交换标识符
-            int flag = 0;
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    flag = 1;
+    public void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            if (flag == 0) {
-                break;
-            }
         }
-    }
-
-    public static void printArray(int[] array) {
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.println("");
     }
 }
